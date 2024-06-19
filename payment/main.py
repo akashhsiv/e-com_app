932f673f575f55cc89143e95952e73ccdbe81b74
akashhsiv <akashhsiv@gmail.com>
2024-06-19 18:57:30 +0530

    editing payment options

---
+++ payment/main.py
@@ -0,0 +1,40 @@
+from fastapi  import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+#this should be a diffrent database
+redis = get_redis_connection(
+    host="redis-19840.c13.us-east-1-3.ec2.redns.redis-cloud.com",
+    port=19840,
+    password="VJoPm7tEdLxfZXZNtH3gB7dwA7CX2H7d",
+    decode_responses=True
+)
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str #completed,pending,refunded
+
+    class Meta:
+        database=redis
+
+@app.post('/orders')
+async def create(request:Request):   #id,quantity
+    body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s'  %body['id'])
+    return req.json()
