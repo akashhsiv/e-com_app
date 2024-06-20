3ec4b76bc6a96fd4bdf97ace03b3e3e0fd624166
akashhsiv <akashhsiv@gmail.com>
2024-06-20 12:24:33 +0530

    added while loop as a server

--- payment/main.py
+++ payment/main.py
@@ -1,4 +1,4 @@
-from fastapi  import FastAPI
+from fastapi import FastAPI
 from fastapi.background import BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
@@ -10,18 +10,19 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
-    allow_methods=['*'],
-    allow_headers=['*']
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-#this should be a diffrent database
+# this should be a diffrent database
 redis = get_redis_connection(
     host="redis-19840.c13.us-east-1-3.ec2.redns.redis-cloud.com",
     port=19840,
     password="VJoPm7tEdLxfZXZNtH3gB7dwA7CX2H7d",
-    decode_responses=True
+    decode_responses=True,
 )
+
 
 class Order(HashModel):
     product_id: str
@@ -29,30 +30,32 @@ class Order(HashModel):
     fee: float
     total: float
     quantity: int
-    status: str #completed,pending,refunded
+    status: str  # completed,pending,refunded
 
     class Meta:
-        database=redis
+        database = redis
 
-@app.get('/order/{pk}')
+
+@app.get("/order/{pk}")
 def get(pk: str):
     return Order.get(pk)
 
-@app.post('/orders')
-async def create(request:Request, background_tasks:BackgroundTasks):  #id,quantity
+
+@app.post("/orders")
+async def create(request: Request, background_tasks: BackgroundTasks):  # id,quantity
     body = await request.json()
 
     req = requests.get(f'http://localhost:8000/products/{body["id"]}')
 
-    product =req.json()
+    product = req.json()
 
-    order= Order(
-    product_id=body['id'],
-    price=product['price'],
-    fee=0.2* product['price'],
-    total=1.2*product['price'],
-    quantity=body['quantity'],
-    status='pending'
+    order = Order(
+        product_id=body["id"],
+        price=product["price"],
+        fee=0.2 * product["price"],
+        total=1.2 * product["price"],
+        quantity=body["quantity"],
+        status="pending",
     )
     order.save()
 
@@ -60,8 +63,9 @@ async def create(request:Request, background_tasks:BackgroundTasks):  #id,quanti
 
     return order
 
-def order_completed(order:Order):
+
+def order_completed(order: Order):
     time.sleep(5)
-    order.status='completed'
+    order.status = "completed"
     order.save()
-    redis.xadd('order_completed',order.dict(),'*')
+    redis.xadd("order_completed", order.dict(), "*")
