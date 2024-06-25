78fce18c5af33f1179b2551ce261a13a219450a0
akashhsiv <akashhsiv@gmail.com>
2024-06-25 09:10:51 +0530

    created schema for user login

--- backend/users/schemas.py
+++ backend/users/schemas.py
@@ -1,14 +1,25 @@
 from pydantic import BaseModel,EmailStr
 
+#base schema for user
 class UserBase(BaseModel):
     username:str
     email:EmailStr
 
+#schema for creating a new user
 class UserCreate(UserBase):
     password:str
 
+#schema for displaying
 class User(UserBase):
     id:int
 
     class Config:
         orm_model = True
+
+#schemas for user login
+class UserLogin(BaseModel):
+    email:EmailStr
+
+class Token(BaseModel):
+    access_token: str
+    token_type :str
